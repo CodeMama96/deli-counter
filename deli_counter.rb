7aba0dcad045aeaaa1ea0d73bf6fb838fd4b4e54
Alicia Weenum <weenum.alicia@gmail.com>
2020-10-22 16:35:49 -0400

    commit tree 242e4dbdc700e94a333ef41d4fd198e66729d1a2

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,23 +1,38 @@
 def line(katz_deli)
     # if line is empty, then say...
-    if katz_deli.length = 0
+    if katz_deli.length == 0 #.empty?
         puts "The line is currently empty."
-        new_line = []
-    else "The line is currently: "
-    return "The line is currently: 1. #{[0]}, 2. #{[1]}, 3. #{[2]}, " #whats going to pull out the names from the array line?
-    end
-    katz_deli
-end 
+    else 
+    # new_line = []
+    # whats going to pull out the names from the array line?
+    # its spot in line abd tgeb add tgat to statement above.
+    status = ["The line is currently:"]
+    katz_deli.each_with_index do |name, index|
+        status.push("#{index+ 1}. #{name}")
+   end 
+    # current_order = [index_other_deli]
+  
+   puts status.join(" ")
+   end
+end
 
 def take_a_number(katz_deli, name)
-    #there's already people in the line and need to add more
-    #add multiple people in a row
+    # name 
+    katz_deli << (name)
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+    # there's already people in the line and need to add more
+    # add multiple people in a row
 end
+
+
 
 def now_serving(katz_deli)
     # var i = who is in line
     #puts "Currently serving #{name}." 
-    if now_serving = 0
+        if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
-    end
+        else
+    puts "Currently serving #{katz_deli[0]}."
+        end 
+    katz_deli.shift
 end

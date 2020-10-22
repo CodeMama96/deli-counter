5661974ea4783c42c422d8248429ac9698ddd888
Alicia Weenum <weenum.alicia@gmail.com>
2020-10-22 12:08:55 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def line(katz_deli)
+    # if line is empty, then say...
+    if katz_deli.length = 0
+        puts "The line is currently empty."
+        new_line = []
+    else "The line is currently: "
+    return "The line is currently: 1. #{[0]}, 2. #{[1]}, 3. #{[2]}, " #whats going to pull out the names from the array line?
+    end
+    katz_deli
+end 
+
+def take_a_number(katz_deli, name)
+    #there's already people in the line and need to add more
+    #add multiple people in a row
+end
+
+def now_serving(katz_deli)
+    # var i = who is in line
+    #puts "Currently serving #{name}." 
+    if now_serving = 0
+    puts "There is nobody waiting to be served!"
+    end
+end
